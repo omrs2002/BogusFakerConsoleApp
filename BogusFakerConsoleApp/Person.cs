08062384e667ccd2165d23546a194e3466aaa120
Omar Abuhadid <omrs2002@yahoo.com>
2022-10-25 13:42:11 +0300

    Add project files.

---
+++ BogusFakerConsoleApp/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BogusFakerConsoleApp
+{
+    public class InvestorPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+    }
+
+
+}
---
+++ BogusFakerConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bogus;
+using BogusFakerConsoleApp;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+int ids = 0;
+Randomizer.Seed = new Random(86753);
+
+Console.WriteLine("{");
+while (ids < 100)
+{
+    ids++;
+    var ff = new Faker<InvestorPerson>()
+          .RuleFor(o => o.Id, f => ids)
+          .RuleFor(o => o.Name, f => f.Name.FullName())
+          .RuleFor(o => o.Email, f => f.Person.Email)
+          .RuleFor(o => o.Mobile, f => f.Person.Phone)
+          .RuleFor(o => o.Website, f => f.Person.Company.Name)
+          .RuleFor(o => o.Phone, f => f.Person.Phone);
+
+    var ss = JsonSerializer.Serialize(ff.Generate());
+
+    Console.WriteLine(ss+",");
+}
+Console.WriteLine("}");
+Console.ReadLine();
+
